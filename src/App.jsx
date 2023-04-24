9d89275708a1a99356048071dd34e3ae2e18c68c
Son Dao <udemydaohsn@gmail.com>
2023-04-24 16:09:53 -0700

    modify .eslintrc.cjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import Welcome from "./components/welcome";
+
 export default function App() {
+  const [number, setNumber] = useState(10);
   return (
     <>
       <Welcome />
